42c7924d8b4fd7eec182d4dd5eb5224d088e5c6e
Benjamin Walkenhorst <krylon@posteo.de>
2026-05-23 20:10:40 +0200

    parser - switch back to peg

--- go.mod
+++ go.mod
@@ -7,5 +7,3 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/logutils v1.0.0
 )
-
-require github.com/alecthomas/participle/v2 v2.1.4
---
+++ parser/parser_test.go
@@ -0,0 +1,20 @@
+// /home/krylon/go/src/github.com/blicero/capybara/parser/parser_test.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 20. 05. 2026 by Benjamin Walkenhorst
+// (c) 2026 Benjamin Walkenhorst
+// Time-stamp: <2026-05-23 19:51:09 krylon>
+
+package parser
+
+import "testing"
+
+func TestSimple(t *testing.T) {
+	expr := "5 + 3"
+	capy := &Parser{Buffer: expr}
+	capy.Init()
+	if err := capy.Parse(); err != nil {
+		t.Fatal(err)
+	}
+
+	capy.PrintSyntaxTree()
+} // func TestSimple(t *testing.T)
